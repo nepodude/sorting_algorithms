b8a56feb5067094e5cff19845b6d952f56405d4d
nepodude <nepo.jean73@gmail.com>
2024-10-28 12:19:07 +0200

    debugged

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,12 +23,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				while (k < size - 1)
-				{
-					my_printf("%d, ", array[k]);
-					k++;
-				}
-				my_printf("%d\n", array[k]);
+                print_array(array, size);
+                k++;
 			}
 			j++;
 		}
