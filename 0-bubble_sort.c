d7e56af541d9ffce0b933b2daaf5eb547d632e5d
nepodude <nepo.jean73@gmail.com>
2024-10-28 10:39:22 +0200

    debugged

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,7 +8,7 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t j, i;
+    size_t i, j, k;
     int temp;
 
     if (size == 0 || size == 1)
@@ -19,14 +19,21 @@ void bubble_sort(int *array, size_t size)
     i = 0;
     while (array && i < size)
     {
-        j = 0
+        j = 0;
         while (j < size - 1)
         {
+            k = 0;
             if (array[j] > array[j + 1])
             {
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
+                while (k < size - 1)
+                {
+                    printf("%d, ", array[k]);
+                    k++;
+                }
+                printf("%d\n", array[k]);
             }
             j++;
         }
