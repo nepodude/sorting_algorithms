f30edd934353fa7656ee207aeaf07ae65adcc734
nepodude <nepo.jean73@gmail.com>
2024-10-28 18:23:11 +0200

    message

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,12 +1,11 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers
- in ascending order using the
- * Selection sort algorithm.
+ * selection_sort - Sorts an array of integers.
  * @array: The array to be sorted.
  * @size: The size of the array.
  */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_index;
