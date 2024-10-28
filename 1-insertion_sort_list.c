4c3a7b9b114770c5e51de407def2375e599d5c8e
nepodude <nepo.jean73@gmail.com>
2024-10-28 16:05:47 +0200

    gotcha

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,7 +7,7 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *next_node;
+	listint_t *current, *next_node, *insert;
 
 	if (!list || !*list || !(*list)->next)
 		return;
@@ -16,9 +16,11 @@ void insertion_sort_list(listint_t **list)
 	while (current)
 	{
 		next_node = current->next;
-		while (current->prev && current->n < current->prev->n)
+		insert = current->prev;
+
+		while (insert && current->n < insert->n)
 		{
-			swap_nodes(list, current->prev, current);
+			swap_nodes(list, &insert, current);
 			print_list((const listint_t *)*list);
 		}
 		current = next_node;
@@ -26,27 +28,24 @@ void insertion_sort_list(listint_t **list)
 }
 
 /**
- * swap_nodes - swaps two adjacent nodes in a doubly linked list.
- * @list: pointer to the head of the list.
- * @node1: first node to swap.
- * @node2: second node to swap.
+ * swap_nodes - Swaps two adjacent nodes in a doubly linked list.
+ * @list: Double pointer to the head of the list.
+ * @n1: Double pointer to the first node to swap.
+ * @n2: Pointer to the second node to swap.
  */
-void swap_nodes(listint_t **list, listint_t *node1, listint_t
-*node2)
+void swap_nodes(listint_t **list, listint_t **n1, listint_t *n2)
 {
-    if (!node1 || !node2)
-		return;
+	(*n1)->next = n2->next;
+	if (n2->next)
+		n2->next->prev = *n1;
 
-	if (node1->prev)
-		node1->prev->next = node2;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev)
+		(*n1)->prev->next = n2;
 	else
-		*list = node2;
+		*list = n2;
 
-	if (node2->next)
-		node2->next->prev = node1;
-
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node1->prev = node2;
-	node2->next = node1;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
